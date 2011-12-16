d0c022107ddc669066cb996e7ec4f466d3cee148
Yury Alekseev <yury_alekseev@yahoo.com>
2011-12-16 17:38:16 +0400

    Add process_lean_file.sh to run foma_helper.py script.

--- process_learn_file.sh
+++ process_learn_file.sh
@@ -1,5 +1,11 @@
-if [ $# -lt 1 ]; then
-    echo 'Usage: process_learn_file <path to flookup binary>';
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo 'Usage: process_learn_file <path to flookup binary>';
 else
-    cat spanish/spanish.txt.learn | awk -F ' ' '{print $1}' | $1 spanish.bin | awk 'NF {print $0}' | python guessforms_reduce.py > spanish_learn.solved;
+  tmp_file=$(mktemp);
+  python foma_helper.py spanish/spanish.txt.learn $tmp_file;
+  cat $tmp_file | python guessforms_reduce.py > spanish_learn.solved;
+  rm -f $tmp_file;
+  #cat spanish/spanish.txt.learn | awk -F ' ' '{print $1}' | $1 spanish.bin | awk 'NF {print $0}' | python guessforms_reduce.py > spanish_learn.solved;
 fi
